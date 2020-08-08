f2853f35ffd4c45530ee301e944b236ceae8643d
anshu06468 <anshukumar.kumar447@gmail.com>
2020-08-08 18:49:28 +0530

    pagination model fetching

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from .models import Listings
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
 def index(request):
-    return render(request,"listings/listings.html")
+    listings = Listings.objects.order_by("-list_date").filter(is_published=True)
 
-def listing(request):
-    return render(request,"listings/listing.html")
+    paginator=Paginator(listings,26)
+    page = request.GET.get('page')
+    paged_listings= paginator.get_page(page)
+
+    context = {
+        "listings":paged_listings
+    }
+    return render(request, "listings/listings.html", context)
+
+def listing(request,listing_id):
+    return render(request, "listings/listing.html")
 
 def search(request):
-    return render(request,"listings/search.html")
+    return render(request, "listings/search.html")
